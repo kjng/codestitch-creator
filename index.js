c396e281c5b36bd7fa6d45fdc3dd2c43b8029388
Kevin Jang <pianistkevinjang@gmail.com>
2017-05-24 21:46:19 -0700

    Complete version 1 of codestitch generator using prompt and nightmare

---
+++ index.js
@@ -0,0 +1,112 @@
+const Nightmare = require('nightmare');
+const prompt = require('prompt');
+
+// Instantiate nightmare
+const nightmare = Nightmare();
+
+// Delay (ms) for page switches. Increase if it is too low!
+const delay = 2000;
+
+const url = 'https://codestitch.io/pads';
+let email = '';
+let password = '';
+let padsToGenerate = 0;
+let generatedPads = [];
+
+async function runner() {
+  console.log('\n----------------------------')
+  console.log('Codestitch Pad Generator version 1.0.0');
+  console.log('Created by Kevin Jang');
+  console.log('----------------------------\n');
+
+  await promptForDetails();
+  await codestitchLogin();
+
+  console.log('Generating pads...');
+
+  generatePads(padsToGenerate)
+    .then(() => {
+        console.log('\nDone!\n');
+        console.log('Here are the generated pads:');
+        console.log('----------------------------');
+        console.log(generatedPads.join('\n'));
+        process.exit();
+      });
+}
+
+function promptForDetails() {
+  console.log('Enter your Codestitch details.');
+
+  prompt.start();
+  return new Promise((resolve, reject) => {
+    prompt.get([
+      {
+        name: 'email',
+        required: true
+      },
+      {
+        name: 'password',
+        required: true,
+        hidden: true
+      },
+      {
+        name: 'padsToGenerate',
+        type: 'integer',
+        required: true,
+      }
+    ],
+    function(err, results) {
+      if (err) { reject(err); }
+
+      email = results.email;
+      password = results.password;
+      padsToGenerate = results.padsToGenerate;
+
+      console.log(`\nLogging in with details: ${email}, ${'*'.repeat(password.length)}`);
+
+      resolve(email, password, padsToGenerate);
+    });
+  });
+}
+
+function codestitchLogin() {
+  return new Promise((resolve, reject) => {
+    nightmare
+      .goto(url)
+      .type('#user_email', email)
+      .type('#user_password', password)
+      .click('input[value="Sign in"') // not as dynamic
+      .wait(delay)
+      .url()
+      .then((url) => {
+        if (url === 'https://codestitch.io/pads') {
+          console.log('Login successful!\n');
+          resolve(url);
+        } else {
+          console.log(`\n:( Login failed. URL is ${url}\nExiting...\n`);
+          process.exit();
+        }
+      });
+  });
+}
+
+async function generatePads(n) {
+  for (let i = 0; i < n; i++) {
+    generatedPads.push(await generatePad(i));
+  }
+}
+
+function generatePad(i) {
+  return new Promise((resolve, reject) => {
+    nightmare.goto('https://codestitch.io/pads/new')
+    .wait(delay)
+    .url()
+    .then((url) => {
+      console.log(`Pad number ${i+1} generated!`);
+      resolve(url);
+    });
+  });
+}
+
+// Runs main function
+runner();
